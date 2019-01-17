702df5f74078e82f10e318be8c35b88058308e63
Tuchnyak <georgii.o.sv@gmail.com>
2019-01-17 09:17:27 +0300

    Activities files added

---
+++ app/src/main/java/com/tuchnyak/smartcriteria/ProjectFormActivity.java
@@ -0,0 +1,13 @@
+package com.tuchnyak.smartcriteria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_form);
+    }
+}
